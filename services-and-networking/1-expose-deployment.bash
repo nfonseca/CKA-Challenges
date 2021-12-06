6fe76d92a57a868437bcce9619fe20985e6d4ec2
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2021-12-06 13:41:00 +0000

    Update 1-expose-deployment.bash

--- services-and-networking/1-expose-deployment.bash
+++ services-and-networking/1-expose-deployment.bash
@@ -3,3 +3,5 @@ kubectl create deployment wordpress --image=wordpress
 
 # expose the deployment
 kubectl expose deployment wordpress  --type=NodePort --port=80 --name=wordpress-service
+
+# testing - just open a web-browser and point to  http://nodeIP:31460
