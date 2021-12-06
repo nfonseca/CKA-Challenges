4112542a62597b068ce8b49b99379abec0df75ab
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2021-12-06 13:38:20 +0000

    Update 1-expose-deployment.bash

--- services-and-networking/1-expose-deployment.bash
+++ services-and-networking/1-expose-deployment.bash
@@ -1,1 +1,5 @@
+# create the deplyment
 kubectl create deployment wordpress --image=wordpress
+
+# expose the deployment
+kubectl expose deployment wordpress  --type=NodePort --port=80 --name=wordpress-service
