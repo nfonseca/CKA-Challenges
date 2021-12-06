57f92954b7c51674947d0834c967fc2c6b1a7d1b
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2021-12-06 11:28:29 +0000

    Update 1-service-account.bash

--- cluster-architecture-installation-configuration/1-service-account.bash
+++ cluster-architecture-installation-configuration/1-service-account.bash
@@ -1,4 +1,4 @@
 kubectl create namespace dev
 kubectl -n dev create serviceaccount tester
 kubectl -n dev create role pod-tester --verb=create --verb=list --verb=delete --resource=pods,services
-kubectl -n dev create rolebinding pod-tester --clusterrole=pod-tester --serviceaccount=dev:tester
+kubectl -n dev create rolebinding pod-tester --role=pod-tester --serviceaccount=dev:tester
