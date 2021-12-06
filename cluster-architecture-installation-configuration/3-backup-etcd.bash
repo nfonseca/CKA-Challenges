3c8f97cb140f22f1ae41b9b701ae785fef7b9248
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2021-12-06 12:09:13 +0000

    Update 3-backup-etcd.bash

--- cluster-architecture-installation-configuration/3-backup-etcd.bash
+++ cluster-architecture-installation-configuration/3-backup-etcd.bash
@@ -1,4 +1,7 @@
 # get the ip of the etcd pod
 kubectl -n kube-system get pods -o wide
 # from the master host
-sudo ETCDCTL_API=3 etcdctl --endpoints 192.168.101.101:2379 --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save snapshotdb
+sudo ETCDCTL_API=3 etcdctl --endpoints 192.168.101.101:2379 --cert=/etc/kubernetes/pki/etcd/server.crt \\
+--key=/etc/kubernetes/pki/etcd/server.key \\
+--cacert=/etc/kubernetes/pki/etcd/ca.crt \\
+snapshot save snapshotdb
