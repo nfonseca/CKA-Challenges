61cefe675d3378c03d2b3ec2494ad1cc91927430
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2021-12-06 11:31:43 +0000

    Update 1-service-account.bash

--- cluster-architecture-installation-configuration/1-service-account.bash
+++ cluster-architecture-installation-configuration/1-service-account.bash
@@ -2,11 +2,3 @@ kubectl create namespace dev
 kubectl -n dev create serviceaccount tester
 kubectl -n dev create role pod-tester --verb=create --verb=list --verb=delete --resource=pods,services
 kubectl -n dev create rolebinding pod-tester --role=pod-tester --serviceaccount=dev:tester
-# check permissions
-
-kubectl -n dev auth can-i create services --as system:serviceaccount:dev:tester
-# yes
-kubectl -n dev auth can-i create pods --as system:serviceaccount:dev:tester
-# yes
-kubectl -n dev auth can-i create secrets --as system:serviceaccount:dev:tester
-# no
