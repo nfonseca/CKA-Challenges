3bfb30d50a32eccca7f3833fe367219a679696a1
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2021-12-09 11:21:21 +0000

    Update 4-upgrade-cluster.bash

--- cluster-architecture-installation-configuration/4-upgrade-cluster.bash
+++ cluster-architecture-installation-configuration/4-upgrade-cluster.bash
@@ -15,3 +15,14 @@ kubeadm upgrade node
 apt-get install -y --allow-downgrades kubelet=1.19.16-00 kubectl=1.19.16-00
 systemctl daemon-reload
 systemctl restart kubelet
+
+
+# Verify
+
+kubectl get nodes
+
+NAME               STATUS   ROLES    AGE     VERSION
+cluster1-master1   Ready    master   5d16h   v1.19.16
+cluster1-worker1   Ready    <none>   5d16h   v1.19.16
+cluster1-worker2   Ready    <none>   5d16h   v1.19.16
+cluster1-worker3   Ready    <none>   5d16h   v1.19.16
